061e4228408145ef5f7851b5b8e7920e78d77059
Mark Maligalig <maligali@ualberta.ca>
2023-09-18 18:22:30 -0600

    Commented out requests version for step 10 of lab1

--- requests_ver.py
+++ requests_ver.py
@@ -1,6 +1,6 @@
 import requests
 
-print(requests.__version__)
+#print(requests.__version__) # was initially used for Q2 of Lab 1
 
 x = requests.get("https://raw.githubusercontent.com/mark8m/CMPUT404-lab1/main/requests_ver.py")
 
